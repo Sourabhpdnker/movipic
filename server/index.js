9294c59675e80a73f30baa304fc7c0e8c6f1e3c5
Sourabh <137615880+Sourabhpdnker@users.noreply.github.com>
2024-06-07 00:00:59 +0530

    routes foe post and user add .. created some posts.js file for auth,...

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import morgan from "morgan";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import { createPost } from "./controllers/posts.js"
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
 // configurations
@@ -40,10 +43,12 @@ const upload = multer({ storage });
 
 //ROUTES WITH FILE
 app.post("/auth/register", upload.single("picture"), register);  //MIDDLEARE FUNCTION
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // routes 
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP 
 const PORT = process.env.PORT || 6001;
