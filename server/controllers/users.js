e7e0dc75d41dd57865fcdb47396051228aaab1a7
Sourabh <137615880+Sourabhpdnker@users.noreply.github.com>
2024-06-06 21:54:35 +0530

    adding add and remove friend functionality

---
+++ server/controllers/users.js
@@ -0,0 +1,46 @@
+import { promise } from "bcrypt/promises";
+import User from "../models/user";
+
+// READ 
+export const getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await user.findbyId(id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+}
+
+export const getUserFriends = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await user.findbyId(id);
+
+        const friends = await promise.all(
+            user.friends.map((id) => User.findbyId(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath }
+            }
+        );
+        res.status(200).json(formattedFriends);
+
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+//update
+export const addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.findbyId(id);
+        const friend = await User.findbyId(friendId);
+
+
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
