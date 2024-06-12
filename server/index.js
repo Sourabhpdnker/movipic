acb7d12571e0441bbc0f4def27deff0c741f6bed
Sourabh <137615880+Sourabhpdnker@users.noreply.github.com>
2024-06-12 19:59:27 +0530

    frontent setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import postRoutes from "./routes/posts.js"
 import { createPost } from "./controllers/posts.js"
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
-
+import Post from "./models/Post.js";
+import User from "./models/user.js";
+import { users, posts } from "./data/index.js"
 
 // configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -57,5 +59,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`sever port : ${PORT}`));
+
+    //add data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 })
     .catch((error) => console.log(`${error} did not connect `));
